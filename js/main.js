a0a6f18dff4e9f950a2f4e9cbfee4acc3a9d2502
davidetaribelli <davide.taribelli@gmail.com>
2023-04-27 15:07:25 +0200

    recuperato dati negli array

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 //DICHIARO LE VARIABILI
 const gridElement = document.querySelector(".container");
-const btnPlay = document.querySelector(".play")
-
+const btnPlay = document.querySelector(".play");
+const option = document.querySelector("#option");
+let numSquare = [];
+let numArray = [];
 
 // AL CLICK DEL BOTTTONE PLAY MI AGGIUNGE UNA STRINGA VUOTA AL CONTAINER
 btnPlay.addEventListener("click", function(){
-    gridElement.innerHTML="";                   
-    let option = document.querySelector("#option").value;        // IN BASE ALL'OPZIONE SCELTA MI VA A MODIDIFICARE IL "BOX" DELLA FUNZIONE
-    play(option);
+    gridElement.innerHTML="";        
+    // IN BASE ALL'OPZIONE SCELTA MI VA A MODIDIFICARE IL "BOX" DELLA FUNZIONE           
+    let optionEl = option.value;      
+    play(optionEl);
+    gen(optionEl);
+
+    console.log(numSquare);
+    console.log(numArray);
 }); 
 
 
@@ -25,15 +32,44 @@ function play (box){
         const newSquare = document.createElement("div");
         newSquare.classList.add("square");
         newSquare.classList.add(`s-${box}`);
-        newSquare.innerHTML=`${i}`;
+        newSquare.innerHTML = `${i}`;
         gridElement.appendChild(newSquare);
+        numSquare.push(i);
 
-        newSquare.addEventListener("click", function() {
-            console.log("La casella selezionata è la numero:", this.innerHTML);
-            this.classList.toggle("clicked");
-        });
-    }  
-        
+        if(numArray == numSquare){
+            let squareTot = document.querySelectorAll(".square");
+            squareTot.addEventListener("click", function() {
+                this.classList.add("clicked-red");
+            });
+
+        } else {
+
+            newSquare.addEventListener("click", function() {
+                console.log("La casella selezionata è la numero:", this.innerHTML);
+                this.classList.toggle("clicked");
+            });
+        }
+    }         
 }
 
+// FUNZIONE CHE MI PERMETTE DI GENERARE 16 NUMERI CASUALI CHE VANNO DA [1-100], [1-81] E [1-49], in base all'opzione
+function gen (num){
+
+    for (let i = 1; i <= 16; i++){
+        let numGen = Math.floor(Math.random() * num + 1);
+
+        if (numGen == numGen){
+            for (let i = 1; i <= 16; i++){
+                numGen = Math.floor(Math.random() * num +1);
+            }
+        }
+        numArray.push(numGen);
+    }
+
+}
+
+// il numero generato casualmente deve coincidere con il numero presente in una cella
+
+// la cella con il numero se schiacciata diventa rossa e la partita finisce
+// in base alle celle senza bomba cliccate dare un punteggio finale
 
